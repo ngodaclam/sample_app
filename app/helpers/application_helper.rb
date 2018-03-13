b268918c85d8cec61b931e149d169dc6adb52a83
LamND <ngodaclam81195@gmail.com>
2018-03-13 16:28:26 +0700

    add chaper 04

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  #Return full title on a page basic
+  def full_title(page_title = '')
+    base_title = "Ruby on rails tutorial in Sample app"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
