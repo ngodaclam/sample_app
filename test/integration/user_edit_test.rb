bc624d1d7856d8d36797594101f9d9198545f572
lamnd <ngodaclam81195@gmail.com>
2018-03-29 16:33:41 +0700

    add

---
+++ test/integration/user_edit_test.rb
@@ -0,0 +1,31 @@
+require 'test_helper'
+
+class UserEditTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+  def setup
+    @user = users(:micheal)
+  end
+
+  test "successful edit" do
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    name  = "Foo Bar"
+    email = "foo@bar.com"
+    patch user_path(@user), params: {
+        user:
+            {
+                name:  name,
+                email: email,
+                password:              "",
+                password_confirmation: ""
+            }
+    }
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal name,  @user.name
+    assert_equal email, @user.email
+  end
+end
